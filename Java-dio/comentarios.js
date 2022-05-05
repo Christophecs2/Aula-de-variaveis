83c4d795f42f0d568b19fbf381389869e9a110ab
Christophe Costa <costachristophe065@gmail.com>
2022-05-04 22:41:13 -0300

    aula de js dio

---
+++ Java-dio/comentarios.js
@@ -0,0 +1,18 @@
+// var y = 0  comentario de linha
+
+/* comentario de varias linhas
+var z = 4
+var sla = 2222 
+// só pra lembrar msm
+*/
+
+
+// --variaveis são apresentadas com letras minusculas e constantes com letras maiusculas--
+
+
+function soma(a, b) {
+    console.log(a + b)
+    return a + b
+}
+
+soma(3, 6)
